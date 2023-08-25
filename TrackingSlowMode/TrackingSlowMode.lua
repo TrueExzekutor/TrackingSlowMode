a58a0220df4eff0b2770c88880e54cb4e2c62de0
TrueExzekutor <orlov-v@mail.ru>
2023-08-26 01:46:42 +1100

    added graphical timer

--- TrackingSlowMode/TrackingSlowMode.lua
+++ TrackingSlowMode/TrackingSlowMode.lua
@@ -1,6 +1,8 @@
 local frame = CreateFrame("Frame")
 local timer = nil
 local player = nil
+local timerFrame = nil
+local timerText = nil
 
 function TrackingSlowMode_OnLoad(self)
     self:RegisterEvent("CHAT_MSG_CHANNEL")
@@ -10,9 +12,25 @@ function TrackingSlowMode_OnLoad(self)
             if timer <= 0 then
                 print("Timer has ended ^_^")
                 timer = nil
+                timerFrame:Hide()
+            else
+                timerText:SetText(math.ceil(timer))
             end
         end
     end)
+
+    timerFrame = CreateFrame("Frame", nil, ChatFrame1)
+    timerFrame:SetSize(20, 20)
+    timerFrame:SetPoint("TOPRIGHT", ChatFrame1, "TOPRIGHT")
+    timerFrame:SetBackdrop({
+        bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
+        edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
+        tile = true, tileSize = 1, edgeSize = 1,
+        })
+    timerFrame:SetBackdropColor(0, 0, 0, 1)
+
+    timerText = timerFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    timerText:SetPoint("CENTER", timerFrame, "CENTER")
 end
 
 function TrackingSlowMode_OnEvent(self, event, ...)
@@ -22,6 +40,7 @@ function TrackingSlowMode_OnEvent(self, event, ...)
         end
         if arg2 == player then
             timer = 15
+            timerFrame:Show()
         end
     end
 end
