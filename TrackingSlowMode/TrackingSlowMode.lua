b6d7b9831a3f6f9e4810680c22a13e98c1ad656b
TrueExzekutor <orlov-v@mail.ru>
2023-09-05 19:22:18 +1100

    added timer move function

--- TrackingSlowMode/TrackingSlowMode.lua
+++ TrackingSlowMode/TrackingSlowMode.lua
@@ -1,4 +1,3 @@
-local frame = CreateFrame("Frame")
 local timer = nil
 local player = nil
 local timerFrame = nil
@@ -6,11 +5,37 @@ local timerText = nil
 local arg3
 local arg8
 local ProtectFunction = true
+local isLocked = true
 
+local timerFrame = CreateFrame("Frame", "STSMFrame", UIParent)
+timerFrame:SetSize(20, 20)
+timerFrame:SetPoint("TOPRIGHT", ChatFrame1, "TOPRIGHT")
+timerFrame:EnableMouse(true)
+timerFrame:SetMovable(true)
+timerFrame:SetUserPlaced(true)
+timerFrame:SetClampedToScreen(true)
+timerFrame:SetScript("OnMouseDown", function(self, button)
+    if isLocked then
+        return
+    end
+    self:StartMoving()
+end)
+timerFrame:SetScript("OnMouseUp", function(self, button)
+    self:StopMovingOrSizing()
+end)
+timerFrame:SetBackdrop({
+    bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
+    edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
+    tile = true, tileSize = 1, edgeSize = 1,
+    })
+timerFrame:SetBackdropColor(0, 0, 0, 1)
+
+timerText = timerFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+timerText:SetPoint("CENTER", timerFrame, "CENTER")
 
 function TrackingSlowMode_OnLoad(self)
     self:RegisterEvent("CHAT_MSG_CHANNEL")
-    frame:SetScript("OnUpdate", function(self, elapsed)
+    timerFrame:SetScript("OnUpdate", function(self, elapsed)
         if timer ~= nil then
             timer = timer - elapsed
             if timer <= 0 then
@@ -21,19 +46,6 @@ function TrackingSlowMode_OnLoad(self)
             end
         end
     end)
-
-    timerFrame = CreateFrame("Frame", nil, ChatFrame1)
-    timerFrame:SetSize(20, 20)
-    timerFrame:SetPoint("TOPRIGHT", ChatFrame1, "TOPRIGHT")
-    timerFrame:SetBackdrop({
-        bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
-        edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
-        tile = true, tileSize = 1, edgeSize = 1,
-        })
-    timerFrame:SetBackdropColor(0, 0, 0, 1)
-
-    timerText = timerFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-    timerText:SetPoint("CENTER", timerFrame, "CENTER")
 end
 
 function TrackingSlowMode_OnEvent(self, event, ...)
@@ -71,7 +83,35 @@ SlashCmdList["TRACKINGSLOWMODE"] = function(args)
     elseif args == "protect off" then
         ProtectFunction = false
         print("ProtectMode disabled")
+    elseif args == "lock" then
+        if isLocked then
+            print("timerFrame is already locked")
+        else
+            timerFrame:SetScript("OnMouseDown", nil)
+            timerFrame:SetScript("OnMouseUp", nil)
+            isLocked = true
+            print("timerFrame is locked")
+        end
+    elseif args == "unlock" then
+        if not isLocked then
+            print("timerFrame is already unlocked")
+        else
+            timerFrame:SetScript("OnMouseDown", function(self, button)
+                self:StartMoving()
+            end)
+            timerFrame:SetScript("OnMouseUp", function(self, button)
+                self:StopMovingOrSizing()
+            end)
+            isLocked = false
+            timerFrame:Show()
+            print("timerFrame is unlocked")
+        end
+    elseif args == "help" then
+        print("Use [/stsm protect off] to disable protect function")
+        print("Use [/stsm protect on] to enable protect function")
+        print("Use [/stsm unlock] to move the timer with the mouse")
+        print("Use [/stsm lock] to lock the timer position")  
     else
-        print("Invalid command. Usage: /stsm /TrackingSlowMode protect [on|off]")
+        print("Invalid command. Usage: /stsm help")
     end
 end
